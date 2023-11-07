de56f9c4a8d8af7d3c641df553e559e44626d961
ryuji abe <ryuji-abe@Room335.local>
2023-11-07 15:27:40 +0900

    [WAP] API Test Postman

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+const { PrismaClient } = require("@prisma/client");
 const express = require("express");
 const app = express();
 const PORT = 8000;
+
+const prisma = new PrismaClient();
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const posts = await prisma.posts.findMany();
+  return res.json(posts);
+});
+
+app.post("/", async (req, res) => {
+  const { title, body } = req.body;
+  const posts = await prisma.posts.create({
+    data: {
+      title: title,
+      body: body,
+    },
+  });
+  return res.json(posts);
+});
 
 app.listen(PORT, () => {
   console.log("サーバー起動中・・・");
